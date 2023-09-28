d7ce7706941e13dad5e354b94206312f497ddf6c
AdongoJr2 <dev.mosesadongo@gmail.com>
2023-09-28 22:16:56 +0300

    0. Form Validation - Password Strength

---
+++ html_js_validation/0-password-strength.js
@@ -0,0 +1,42 @@
+function validatePassword() {
+  let passwordErrorMessage = "";
+
+  const passwordElement = document.querySelector("#password");
+  const passwordValue = passwordElement?.value;
+
+  if (!passwordValue || passwordValue.length < 8) {
+    // at least 8 characters long
+    passwordErrorMessage = "Password must be at least 8 characters long";
+  } else if (passwordValue === passwordValue.toLowerCase()) {
+    // at least one uppercase letter
+    passwordErrorMessage =
+      "Password must contain at least one uppercase letter";
+  } else if (passwordValue === passwordValue.toUpperCase()) {
+    // at least one lowercase letter
+    passwordErrorMessage =
+      "Password must contain at least one lowercase letter";
+  } else if (!((/\d+/g).test(passwordValue))) {
+    // at least one numeric digit
+    passwordErrorMessage = "Password must contain at least one numeric digit";
+  } else if (!(/[.!@#$%^&*]/.test(passwordValue))) {
+    // at least one special character (e.g., !@#$%^&*)
+    passwordErrorMessage = "Password must contain at least one special character (e.g., !@#$%^&*)";
+  } else {
+    passwordErrorMessage = "";
+  }
+
+  const passwordErrorElement = document.querySelector("#error");
+  passwordErrorElement.textContent = passwordErrorMessage;
+
+  if (!passwordElement) {
+    // allow form submission
+    const form = document.querySelector('#passwordForm');
+    form?.submit();
+  }
+}
+
+const submitBtn = document.querySelector("button[type=submit]");
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  validatePassword();
+});
